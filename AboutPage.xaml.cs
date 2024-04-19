e3acd878b92fa01836a0bc665792102867ff56eb
woodFordR <adam@kaewell.com>
2024-04-19 03:22:39 -0400

    initial commit

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,18 @@
+using Microsoft.Maui.ApplicationModel;
+namespace Notes
+{
+
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LearnMore_Clicked(object sender, EventArgs e)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync("https://aka.ms/maui");
+        }
+    }
+}
